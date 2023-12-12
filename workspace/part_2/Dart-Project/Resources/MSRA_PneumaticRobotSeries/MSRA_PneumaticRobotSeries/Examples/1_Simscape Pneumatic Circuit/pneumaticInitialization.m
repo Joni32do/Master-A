6f2b8eaf4b984f89e3bef607065a6d9bbcd4ac1d
Jonathan <jonathanschnitzler@web.de>
2023-12-12 19:49:21 +0100

    Update Dart Project

---
+++ workspace/part_2/Dart-Project/Resources/MSRA_PneumaticRobotSeries/MSRA_PneumaticRobotSeries/Examples/1_Simscape Pneumatic Circuit/pneumaticInitialization.m
@@ -0,0 +1,14 @@
+%% Pneumatic System Initialization 
+% Copyright 2018 The MathWorks, Inc.
+
+%% Simulation Parameters 
+A_pipe = 5e-4;
+A_piston = 0.0020;
+A_valve_max = 4e-4;
+A_valve_min = 1e-10;
+delta_p = 0.6;
+L_pipe = 1;
+L_piston = 0.2;
+p_init = 0.1013;
+T_atm = 293.15;
+T_init = 293.1500;
---
+++ workspace/part_2/Dart-Project/Resources/MSRA_PneumaticRobotSeries/MSRA_PneumaticRobotSeries/initialize.m
@@ -0,0 +1,5 @@
+% Modeling Pneumatic Robot Actuators -- Initialization script
+% Copyright 2018 The MathWorks, Inc.
+
+% Add necessary folders to the MATLAB path
+addpath(genpath('Examples'),genpath('Libraries'));
